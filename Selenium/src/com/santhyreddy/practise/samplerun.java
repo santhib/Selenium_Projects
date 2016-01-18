06b4451ccfa29dd4f6e617ee891ffc69c8720d61
Santhy Reddy <santhyreddy@gmail.com>
2016-01-18 10:25:21 -0500

    Create samplerun.java
    Sample first run

---
+++ Selenium/src/com/santhyreddy/practise/samplerun.java
@@ -0,0 +1,11 @@
+package com.santhyreddy.practise;
+
+public class Samplerun {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Selenium project");
+
+	}
+
+}
